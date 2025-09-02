d26dfddd34f6057afd8aabe87c8f759b910253cc
JuanJo1428 <juvasquezco@unal.edu.co>
2025-09-02 13:27:03 -0500

    Ejercicios Actividad Evaluativa 1

---
+++ Ejercicio Resuelto 14.py
@@ -0,0 +1,28 @@
+# Ejercicio Número 14
+
+class CC:
+    
+    @staticmethod
+    def CalcularCuadrado(Numero):
+        Cuadrado = pow(Numero, 2)
+        return Cuadrado
+    
+    @staticmethod
+    def CalcularCubo(Numero):
+        Cubo = pow(Numero, 3)
+        return Cubo
+    
+class Ejercicio_14:
+    
+    def Valores():
+        
+        Numero = float(input("Ingrese el Número del cual desea conocer su Cuadrado y su Cubo: "))
+
+        Cuadrado = CC.CalcularCuadrado(Numero)
+        Cubo = CC.CalcularCubo(Numero)
+        
+        print(f"El Cuadrado y el Cubo del número {Numero} son: Cuadrado: {Cuadrado}, Cubo: {Cubo}.")
+
+
+Ejercicio_14.Valores()
+
